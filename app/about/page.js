be21634068a965bb63b6bd1a6e99df3469fa7ab6
Dalliaissleeping <dalliavuongle@gmail.com>
2025-10-29 00:06:29 -0500

    hofjweo

--- app/about/page.js
+++ app/about/page.js
@@ -10,7 +10,9 @@ export default function AboutPage() {
 
 
   return (
+    //added the spacing betweed h1 and p and also increased the font size of h1
     <div className= "header">
+
       <h1 style = {{fontSize: "32px", lineHeight: "1.8"}}>About</h1>
 
       <p>Hi there! We are a team of five, Mia, Dallia, Uy, Nykaela, and William. We built this website for those 
