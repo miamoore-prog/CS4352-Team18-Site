a4755ef7f58c3abbc3c2cceb88e1629e04e57606
Dalliaissleeping <dalliavuongle@gmail.com>
2025-10-28 23:55:20 -0500

    about 3

--- app/about/page.js
+++ app/about/page.js
@@ -5,12 +5,17 @@
 import React from "react";
 
 export default function AboutPage() {
-    const a = 1;
+  
+
+
 
   return (
     <div className= "header">
-      <h1>About</h1>
-      <p>My supercool header</p>
+      <h1 id="About_title">About</h1>
+
+      <p>Hi there! We are a team of five, Mia, Dallia, Uy, Nykaela, and William. We built this website for those 
+        who want to learn more about new technologies and how they can be used in everyday life. From AI to simply new apps, we've got you convered! 
+      </p>
     </div>
   );
 };
