781826d19ba39dd4dab4f4077bcfff40faed669c
Dalliaissleeping <dalliavuongle@gmail.com>
2025-10-29 15:22:53 -0500

    hgyuhjb

--- app/about/page.js
+++ app/about/page.js
@@ -79,7 +79,7 @@ export default function AboutPage() {
  {/* Home Button */}
  <Link href="/">
         <h1 style = {{lineHeight: "1.8"}}> </h1>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" style={{lineHeight: "1.8"}}>
+        <button className= "px-4 py-2 bg-purple-600 text-white rounded hover:bg-blue-100" style={{lineHeight: "1.8"}}>
           Home
         </button>
       </Link>
