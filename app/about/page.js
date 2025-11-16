aaca2114a8d80de0bb005e4193efa91ee6ddf1d0
wagabi12 <99624866+wagabi12@users.noreply.github.com>
2025-11-15 18:39:40 -0600

    Fixed UI issues in tools modal and about page (#41)
    * Added fix to stop home page from scrolling after opening tool modal.
    Fixed issue where clicking how to guide would over flow page.
    Textbox for reviews is now hidden till user clicks add/update.
    
    * Got rid of home button

--- app/about/page.js
+++ app/about/page.js
@@ -77,16 +77,6 @@ export default function AboutPage() {
 
 
 
- {/* Home Button */}
- <Link href="/">
-        <h1 style = {{lineHeight: "1.8"}}> </h1>
-        <button className= "px-4 py-2 bg-purple-600 text-white rounded hover:bg-blue-100" style={{lineHeight: "1.8"}}>
-          Home
-        </button>
-      </Link>
-
-
-
 
 
     </div>
